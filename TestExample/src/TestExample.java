42a65501034ba567b88f97802896b8c4b14dfb97
oquintela <oquintela@altimetrik.com>
2013-02-20 14:40:01 -0200

    test

---
+++ TestExample/src/TestExample.java
@@ -0,0 +1,19 @@
+
+
+
+/**
+ * 
+ * @author QATeam
+ *
+ */
+public class TestExample{
+	  //testxcXZC
+	  //test2dds
+	//adsfafads
+	//sss2
+//change
+	
+	//latest branch
+	//sss///sss
+	//ssss
+}
---
+++ TestExample/src/com/template/tests/example/TestExample.java
@@ -0,0 +1,35 @@
+package com.template.tests.example;
+
+
+
+/**
+ * 
+ * @author QATeam
+ *
+ */
+public class TestExample{
+<<<<<<< HEAD
+	  //testxcXZC
+	  //test2dds
+	//adsfafads
+	//sss2
+//change
+	
+	//latest branch
+	//sss///sss
+	//ssss
+}
+=======
+        //Test
+
+	  //testxcXZC
+	  //test2dds
+	//adsfafads
+	//sss2
+//change
+	
+	//latest branch
+	//sss///sss
+	//ssss
+}
+>>>>>>> branch 'master' of https://github.com/oquintelaaltimetrik/test.git
